b0eb6e0020bd11e5a9299681661047eb7baf4b7a
Danny Lindquist <danny.lindquist@tanium.com>
2023-03-06 08:22:26 -0800

    add transaction create

--- app/routes/transactionRoutes.tsx
+++ app/routes/transactionRoutes.tsx
@@ -1,4 +1,9 @@
 import { getUserCategories } from "../db/categoryService.js";
+import {
+  createTransaction,
+  TransactionInsert,
+  transactionSchema,
+} from "../db/transactionService.js";
 import { Router } from "../router.js";
 import { renderView } from "../utils/renderView.js";
 import { TransactionEditPage } from "../views/TransactionEditPage.jsx";
@@ -14,9 +19,24 @@ export function registerTransactionRoutes(router: Router) {
   router.post("/transactions/new", async (request, context) => {
     const user = context.props.get("user");
     const formData = await request.formData();
-    const description = formData.get("description");
-    const amount = formData.get("amount");
-    const data = Object.fromEntries(formData.entries());
-    return new Response(JSON.stringify(data));
+    const formObject = Object.fromEntries(formData.entries());
+    const transaction = transactionSchema.safeParse(formObject);
+    if (transaction.success) {
+      const date = new Date(transaction.data.date);
+      const toInsert: TransactionInsert = {
+        amount: Math.floor(transaction.data.amount * 100),
+        user: user.id,
+        category: transaction.data.category,
+        date: Math.floor(date.getTime() / 1000),
+        description: transaction.data.description,
+        is_withdrawal: 1,
+        month_key: +`${date.getUTCFullYear()}${(date.getUTCMonth() + 1)
+          .toString()
+          .padStart(2, "0")}`,
+      };
+      const newTransaction = createTransaction(toInsert);
+      return new Response(JSON.stringify(newTransaction));
+    }
+    return new Response(JSON.stringify(transaction.error));
   });
 }
